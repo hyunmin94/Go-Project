595fd2553b507d611941039c019486d62db48b9f
hyunmin94 <https://github.com/hyunmin94>
2021-01-03 17:57:42 +0900

    [기본문법] 변수 설명 추가

--- basic/var_and_const.go
+++ basic/var_and_const.go
@@ -11,6 +11,9 @@ func main() {
 
 	// 2. Variables(변수)는 값을 변경할 수 있는 수를 의미한다.
 	var name2 string = "hyunmin"
+	// * func(함수)안에서는 변수를 선언할때 타입을 지정하지 않아도 자동으로 초기화 작업에 의해 타입이 정해진다.
+	// * var name2 string = "hyunmin" 와 동일
+	// name2 := "hyunmin"
 	name2 = "sangjun"
 
 	// 3. 결과 확인
